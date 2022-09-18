9c0f5e904a7fdd96dc1a539fd5ed2119f84b0bf0
Erik <101059428+ErikVu95@users.noreply.github.com>
2022-09-18 23:53:56 +0200

    Initial commit

---
+++ JsonOppgaveFraTerje/CountyList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonOppgaveFraTerje
+{
+    public class CountyList
+    {
+        public string Name { get; set; }
+        public int IdNumber { get; set; }
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        //public int Number3 { get; set; }
+        //public int Number4 { get; set; }
+        //public int Number5 { get; set; }
+        //public int Number6 { get; set; }
+
+        public CountyList(int idnumber, string name, int number1, int number2)
+        {
+            IdNumber = idnumber;
+            Name = name;
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+        //public CountyList(int idnumber, string name, int number1, int number2, int number3, int number4)
+        //{
+        //    IdNumber = idnumber;
+        //    Name = name;
+        //    Number1 = number1;
+        //    Number2 = number2;
+        //    Number3 = number3;
+        //    Number4 = number4;
+        //}
+
+        //public CountyList(int idnumber, string name, int number1, int number2, int number3, int number4, int number5, int number6)
+        //{
+        //    IdNumber = idnumber;
+        //    Name = name;
+        //    Number1 = number1;
+        //    Number2 = number2;
+        //    Number3 = number3;
+        //    Number4 = number4;
+        //    Number5 = number5;
+        //    Number6 = number6;
+        //}
+
+
+    }
+}
---
+++ JsonOppgaveFraTerje/SortWorkShopAndApprove.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace JsonOppgaveFraTerje
+{
+    public class SortWorkShopAndApprove
+    {
+        public string Bedriftsnavn { get; set; }
+        public string Adresse { get; set; }
+        public string Postnummer { get; set; }
+        public int PostnummerToInt { get; set; }
+        public string Poststed { get; set; }
+        public string Godkjenningstyper { get; set; }
+        public int Organisasjonsnummer { get; set; }
+        public int Godkjenningsnummer { get; set; }
+
+        public SortWorkShopAndApprove(string bedriftsnavn, string adresse, string postnummer, string poststed, string godkjenningstyper, int organisasjonsnummer, int godkjenningsnummer)
+        {
+            Bedriftsnavn = bedriftsnavn;
+            Adresse = adresse;
+            Postnummer = postnummer;
+            Poststed = poststed;
+            Godkjenningstyper = godkjenningstyper;
+            Organisasjonsnummer = organisasjonsnummer;
+            Godkjenningsnummer = godkjenningsnummer;
+            CheckParse();
+        }
+
+        public void CheckParse()
+        {
+            if (Postnummer != "")
+            {
+                PostnummerToInt = int.Parse(Postnummer);
+            }
+        }
+    }
+}
